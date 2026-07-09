2fe1ba3f97186e33d0ab078e1c6cb48dc4c0378c
rp1s <rp1s@users.noreply.github.com>
2026-07-09 22:57:58 +0300

    add fn build + update art

--- core/cli/handler.go
+++ core/cli/handler.go
@@ -10,17 +10,20 @@ import (
 type Command func() error
 
 var commands = map[string]Command{
-	// "build": Build,
-	"help": Help,
+	"build": Build,
+	"help":  Help,
 }
 
 func HandlerCmd() error {
-	if len(os.Args) < 1 {
-		cmd := commands[os.Args[1]]
-		return cmd()
-
+	if len(os.Args) < 2 {
+		return Help()
 	}
-	cmd := commands[os.Args[1]]
+
+	cmd, ok := commands[os.Args[1]]
+	if !ok {
+		return Help()
+	}
+
 	return cmd()
 }
 
@@ -37,6 +40,20 @@ func Help() error {
 	sb := &strings.Builder{}
 	cls := colorista.NewColorista(colorista.ThemeAuto)
 
+	candyGradientArt := []colorista.GradientPos{
+		{Pos: 0.00, Color: colorista.RGB{R: 255, G: 70, B: 165}},  // bubblegum
+		{Pos: 0.10, Color: colorista.RGB{R: 255, G: 120, B: 220}}, // cotton candy
+		{Pos: 0.20, Color: colorista.RGB{R: 210, G: 90, B: 255}},  // grape
+		{Pos: 0.30, Color: colorista.RGB{R: 120, G: 90, B: 255}},  // violet blue
+		{Pos: 0.40, Color: colorista.RGB{R: 70, G: 170, B: 255}},  // blue raspberry
+		{Pos: 0.50, Color: colorista.RGB{R: 60, G: 240, B: 255}},  // ice cyan
+		{Pos: 0.60, Color: colorista.RGB{R: 90, G: 255, B: 210}},  // mint
+		{Pos: 0.70, Color: colorista.RGB{R: 170, G: 255, B: 130}}, // lime
+		{Pos: 0.80, Color: colorista.RGB{R: 255, G: 245, B: 90}},  // lemon
+		{Pos: 0.90, Color: colorista.RGB{R: 255, G: 175, B: 90}},  // peach
+		{Pos: 1.00, Color: colorista.RGB{R: 255, G: 90, B: 150}},  // strawberry
+	}
+
 	candyGradient := []colorista.GradientPos{
 		{Pos: 0.00, Color: colorista.RGB{R: 255, G: 80, B: 180}},  // pink
 		{Pos: 0.20, Color: colorista.RGB{R: 255, G: 120, B: 220}}, // candy
@@ -45,7 +62,8 @@ func Help() error {
 		{Pos: 0.80, Color: colorista.RGB{R: 80, G: 255, B: 220}},  // mint
 		{Pos: 1.00, Color: colorista.RGB{R: 255, G: 240, B: 80}},  // yellow
 	}
-	sln(sb, "ART\n\n")
+
+	sln(sb, cls.Gradient(candyArt, candyGradientArt))
 	sln(sb, cls.Gradient("Usage: candy <command> [options]\n", candyGradient))
 
 	sln(sb, cls.Gradient("Commands:", candyGradient))
