a8aa2f1a4ebdbd63761c78aeefd81c67c303befb
rp1s <rp1s@users.noreply.github.com>
2026-07-14 22:01:39 +0300

    add package digerr

---
+++ internal/digerr/p_test.go
@@ -0,0 +1,40 @@
+package diagnostics
+
+import (
+	"os"
+	"testing"
+
+	"github.com/CandyCrafts/candy/internal/digerr/errors"
+	"github.com/CandyCrafts/candy/internal/parser/token"
+)
+
+func TestPrintDiagnostics(t *testing.T) {
+	arena := &Arena{
+		Source: `fn main() {
+print("Hello, World!")
+}`}
+
+	arena.Add(errors.Errors[3].Update(token.Token{
+		Kind: token.D_COLON,
+		Pos: token.Position{
+			FileName: "main.fg",
+			Line:     1,
+			Column:   1,
+			Offset:   0,
+		},
+		Start: 0,
+		End:   2,
+	}))
+
+	if !arena.HasErrors() {
+		t.Errorf("Expected arena to have errors, but it does not.")
+	}
+
+	arena.Print(os.Stderr)
+
+	arena.Clear()
+
+	if arena.HasErrors() {
+		t.Errorf("Expected arena to be cleared of errors, but it still has errors.")
+	}
+}
