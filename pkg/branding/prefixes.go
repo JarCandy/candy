ae948bcec08d0e85e39f70d5b961b093ae312033
rp1s <rp1s@users.noreply.github.com>
2026-07-15 14:01:00 +0300

    update examples for new use syntax

--- pkg/branding/prefixes.go
+++ pkg/branding/prefixes.go
@@ -3,3 +3,9 @@ package branding
 var (
 	PrefixInterfaceFile string = ".ci"
 )
+
+// Parser settings
+var (
+	// If you want to support syntax with db::sqlite::("", conn: "")
+	ExtendedAttrSyntax bool = false
+)
